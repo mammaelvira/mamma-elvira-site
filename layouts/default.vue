6eeb4566db4a7c25f06cae235406c687024b4106
arb85 <arb85@users.noreply.github.com>
2023-03-29 10:57:59 +0200

    feat: :lipstick: Added default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,15 @@
-<template>
-  <div>
-    <BodyTheHeader />
-    layout
+<script setup lang="ts">
+const menuModalOpen = useMenuModalOpen()
+</script>
 
-    <slot />
+<template>
+  <div id="app-layout">
+    <dialog id="full-page-modal" :open="menuModalOpen"></dialog>
+    <BodyTheHeader />
+
+    <main class="p-8 pt-24">
+      <slot />
+    </main>
 
     <BodyTheFooter />
   </div>
